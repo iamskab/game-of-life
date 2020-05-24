8cb57b666094e610cc53dcd2e33d9e78c6b025f7
iamskab <iamsourav470@gmail.com>
2020-05-24 20:42:49 +0530

    Updates

---
+++ precache-manifest.f51e343a2c99d6ef0450110a3f6acaf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a64fd7073e15e16de2c746cfd7b092",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "ecb784d4d647deeecea4",
+    "url": "/game-of-life/static/css/main.4c734f0b.chunk.css"
+  },
+  {
+    "revision": "70b01149f46afcb519c1",
+    "url": "/game-of-life/static/js/2.5f431985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.5f431985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb784d4d647deeecea4",
+    "url": "/game-of-life/static/js/main.a72f105f.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  }
+]);
